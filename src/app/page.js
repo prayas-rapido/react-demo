394504288b591c28145acc033c0eaa56ba7cc934
Jain Prayas <p.jain3322@gmail.com>
2025-02-18 04:03:12 +0530

    Registration form added

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,8 @@
+"use client";
 import { UserCard } from "./card/UserCard";
 import { Counter } from "./counter/counter";
 import { Greetings } from "./greetings";
+import { RegistrationForm } from "./form/Registration";
 
 export default function Home() {
 
@@ -18,7 +20,8 @@ export default function Home() {
     <div>
       {/* {names.map((name, index) => <Greetings key={index} name={name} />)} */}
       {/* <UserCard /> */}
-      <Counter/>
+      {/* <Counter/> */}
+      <RegistrationForm />
     </div>
   );
 }
